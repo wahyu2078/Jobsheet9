97a79980d2fe857df21b19b9c3904e4005b32524
wahyu2078 <wahyutptp20@gmail.com>
2023-10-30 09:08:13 +0700

    Modifikasi_arraybilangan

--- ArrayBilangan29.java
+++ ArrayBilangan29.java
@@ -1,19 +1,16 @@
-/**
- * ArrayBilangan29
- */
+
 public class ArrayBilangan29 {
 
     public static void main(String[] args) {
-    int [] bil = new int[4];
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
+        double[] bil = new double[4];
+        bil[0] = 5.0;
+        bil[1] = 12867;
+        bil[2] = 7.5;
+        bil[3] = 2000000;
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(bil[i]);
+        }
     }
-    
+
 }
