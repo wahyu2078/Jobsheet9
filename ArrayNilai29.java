757cedfe38957a0cc22646bf15cf863599ebe9cd
wahyu2078 <wahyutptp20@gmail.com>
2023-10-30 09:33:43 +0700

    ArrayNilai

--- ArrayNilai29.java
+++ ArrayNilai29.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
+
 public class ArrayNilai29 {
     public static void main(String[] args) {
-        int [] nilaiAkhir = new int [10];
+        int[] nilaiAkhir = new int[10];
         Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < 10; i++){
-            System.out.print("Masukkan nilai akhir ke-"+i+" : ");
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.print("Masukkan nilai akhir ke-" + i + " : ");
             nilaiAkhir[i] = sc.nextInt();
         }
-        for (int i = 0; i < 10; i++){
-            System.out.print("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+
+        for (int i = 0; i < 10; i++) {
+            String status = (nilaiAkhir[i] > 70) ? "lulus" : "tidak lulus";
+            System.out.println("Mahasiswa ke-" + i + " " + status);
         }
     }
 }
