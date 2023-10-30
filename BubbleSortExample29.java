6e3a3b0c087b5d274d6350b17a40d3719e2eddd7
wahyu2078 <wahyutptp20@gmail.com>
2023-10-30 10:15:33 +0700

    Modifikasi_BubbleSortExample

--- BubbleSortExample29.java
+++ BubbleSortExample29.java
@@ -1,13 +1,11 @@
-
 public class BubbleSortExample29 {
-
     public static void main(String[] args) {
         int[] intData = { 34, 18, 87, 72, 32, 54, 43 };
         int temp = 0;
 
         for (int i = 0; i < intData.length; i++) {
             for (int j = 1; j < intData.length - i; j++) {
-                if (intData[j - 1] > intData[j]) {
+                if (intData[j - 1] < intData[j]) { 
                     temp = intData[j];
                     intData[j] = intData[j - 1];
                     intData[j - 1] = temp;
