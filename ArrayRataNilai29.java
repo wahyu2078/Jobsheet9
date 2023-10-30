14af32f66f03d786c0dcd7da51c9401a92c484d0
wahyu2078 <wahyutptp20@gmail.com>
2023-10-30 09:42:10 +0700

    ArrayRataNilai

---
+++ ArrayRataNilai29.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ArrayRataNilai29 {
+    public static void main(String[] args) {
+        int[] nilaiMhs = new int[10];
+        double total = 0;
+        double rata2;
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
+            nilaiMhs[i] = sc.nextInt();
+        }
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            total += nilaiMhs[i];
+        } rata2 = total/nilaiMhs.length;
+        System.out.println("Rata-rata nilai = "+rata2);
+
+    }
+}
